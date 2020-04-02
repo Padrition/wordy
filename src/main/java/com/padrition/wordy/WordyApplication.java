3189793e3e5f99c789c64b17efacc0aae9397d25
Padrition <dimastail23@gmail.com>
2020-04-02 19:47:35 +0600

    first commit

---
+++ src/main/java/com/padrition/wordy/WordyApplication.java
@@ -0,0 +1,13 @@
+package com.padrition.wordy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/padrition/wordy/WordyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padrition.wordy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
