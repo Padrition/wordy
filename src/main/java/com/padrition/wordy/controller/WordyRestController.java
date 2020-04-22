e9809b6129d055b1e482e4907e9b45febda44f87
Padrition <dimastail23@gmail.com>
2020-04-22 18:55:21 +0600

    Added jquery for processing json from rest controller

--- src/main/java/com/padrition/wordy/controller/WordyRestController.java
+++ src/main/java/com/padrition/wordy/controller/WordyRestController.java
@@ -1,8 +1,6 @@
 package com.padrition.wordy.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,11 +1,46 @@
 $(document).ready(function() {
 	$("#search-form").submit(function(event){
-		event.perventDefault();
+		event.preventDefault();
 		
 		fire_ajax_submit();
 	});
 });
 
 function fire_ajax_submit(){
-	
+	$("#definition").html("");
+	var term = $("#word").val();
+	$.ajax({
+		type : "GET",
+		datatype : "json",
+		url : "/api/search/" + term, 
+		success : function(data) {
+			console.log(data);
+			$("#searched-Word").html(data.word);
+			console.log(data.meaning.noun);
+			if(data.meaning.noun !== null){
+				$("#definition").append("<p>[noun]</p>");
+				$("#definition").append("<ul></ul>");
+				$.each(data.meaning.noun , function(i){
+					$("#definition").append("<li>"+data.meaning.noun[i].definition+"</li>");
+				});
+			}
+			if(data.meaning.verb !== null){
+				$("#definition").append("<p>[verb]</p>");
+				$("#definition").append("<ul></ul>");
+				$.each(data.meaning.verb , function(i){
+					$("#definition").append("<li>"+data.meaning.verb[i].definition+"</li>");
+				});
+			}
+			if(data.meaning.adverb !== null){
+				$("#definition").append("<p>[adverb]</p>");
+				$("#definition").append("<ul></ul>");
+				$.each(data.meaning.adverb , function(i){
+					$("#definition").append("<li>"+data.meaning.adverb[i].definition+"</li>");
+				});
+			}
+		},
+		error : function(){
+			console.log("error");
+		}
+	});
 };
